573cdb041c93e2a879217e84f46675d5d4fb0798
groupmicrofin <groupmicrofin@gmail.com>
2017-11-18 13:19:02 +0530

    18-Nov-17: First addition of project

---
+++ mc/app/src/main/java/com/groupmicrofin/moneycalc/moneycalculator/GroupRegistrationActivity.java
@@ -0,0 +1,19 @@
+package com.groupmicrofin.moneycalc.moneycalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GroupRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_registration);
+    }
+
+
+    public void registerGroup(View view){
+
+    }
+}
