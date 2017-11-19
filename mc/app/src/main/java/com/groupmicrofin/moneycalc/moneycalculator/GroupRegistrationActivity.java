ff2a5cac5b7c607e1913da7601ece9f22ccfd569
koulomar <koulomar@gmail.com>
2017-11-19 11:12:18 +0530

    19-Nov-17: Group Registration Main Activity basic completed

--- mc/app/src/main/java/com/groupmicrofin/moneycalc/moneycalculator/GroupRegistrationActivity.java
+++ mc/app/src/main/java/com/groupmicrofin/moneycalc/moneycalculator/GroupRegistrationActivity.java
@@ -3,6 +3,8 @@ package com.groupmicrofin.moneycalc.moneycalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class GroupRegistrationActivity extends AppCompatActivity {
 
@@ -14,6 +16,27 @@ public class GroupRegistrationActivity extends AppCompatActivity {
 
 
     public void registerGroup(View view){
+        String grpNameLabel = getString(R.string.grpName);
 
+        EditText grpRegET = findViewById(R.id.groupRegNo);
+        String grpRegNo = String.valueOf(grpRegET.getText());
+
+        EditText grpNmET = findViewById(R.id.groupName);
+        String grpRegNm = String.valueOf(grpNmET.getText());
+
+        EditText grpSDET = findViewById(R.id.groupStartDate);
+        String grpSD = String.valueOf(grpSDET.getText());
+
+        EditText grpShareAmtET = findViewById(R.id.shareAmount);
+        int grpShareAmt = Integer.parseInt(String.valueOf(grpShareAmtET.getText()));
+
+        EditText grpIntRtET = findViewById(R.id.interestRate);
+        String grpIntRt = String.valueOf(grpIntRtET.getText());
+
+
+        String toastText = "RegNo: "+grpRegNo+" RegNm: "+grpRegNm+grpSD+" ShareAmt: "+grpShareAmt+" Int Rate:"+ grpIntRt;
+
+        Toast ourFirstToast = Toast.makeText(this,toastText,Toast.LENGTH_LONG);
+        ourFirstToast.show();
     }
 }
